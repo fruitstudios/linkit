cb1d9f17c9b1e34409f4d057d98feea09dbe0037
Christian Ruhstaller <christian.ruhstaller@goldinteractive.ch>
2017-02-03 10:31:30 +0100

    add german translations

---
+++ fruitlinkit/translations/de.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Email Address' => 'E-Mail Adresse',
+    'Phone Number' => 'Telefonnummer',
+    'Custom URL' => 'Beliebige URL',
+    'Entry' => 'Eintrag',
+    'Asset' => 'Datei',
+    'Select an asset' => 'Eine Datei auswählen',
+    'Select an entry' => 'Einen Eintrag auswählen',
+    'Select a category' => 'Eine Kateogrie auswählen',
+    'Custom Link Text' => 'Beliebiger Linktext',
+    'Open link in new window?' => 'Link im neuen Fenster öffnen?',
+
+];
